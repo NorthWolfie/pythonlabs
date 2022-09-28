d946e6059e9efea5907973ce3ad3c7b96d4bebb5
NorthWolfie <guy20001@mail.ru>
2022-09-28 16:33:32 +0300

    Загрузка файлов на сервер. Подготовка к работе с лабораторной 3

---
+++ lab3/sort.py
@@ -0,0 +1,16 @@
+def BubbleSort(a):
+    N = len(a)
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+
+
+def InsertionSort(a):
+    for i in range(1, len(a)):
+        temp = a[i]
+        j = i - 1
+        while (j >= 0 and temp < a[j]):
+            a[j + 1] = a[j]
+            j = j - 1
+        a[j + 1] = temp
