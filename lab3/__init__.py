e3ea2ee27230dcc224758a018aebf15319135986
NorthWolfie <guy20001@mail.ru>
2022-10-06 14:52:04 +0300

    Закончил работу с лабораторной №3

--- lab3/__init__.py
+++ lab3/__init__.py
@@ -1,2 +1,4 @@
+import sys
 
-print(f'Файл __init__.py в пакете {__name__}')
+
+
--- lab3/sort.py
+++ lab3/sort.py
@@ -4,13 +4,15 @@ def BubbleSort(a):
         for j in range(N - i - 1):
             if a[j] > a[j + 1]:
                 a[j], a[j + 1] = a[j + 1], a[j]
+    return a
 
 
 def InsertionSort(a):
     for i in range(1, len(a)):
         temp = a[i]
         j = i - 1
-        while (j >= 0 and temp < a[j]):
+        while j >= 0 and temp < a[j]:
             a[j + 1] = a[j]
             j = j - 1
         a[j + 1] = temp
+    return a
